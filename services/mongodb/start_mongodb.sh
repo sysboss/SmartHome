917b95b616a4582bbad95123a0eedee1ea3b2a54
sysboss <sysboss@mail.ru>
2016-09-03 18:34:22 +0300

    mongodb start script fix

--- services/mongodb/start_mongodb.sh
+++ services/mongodb/start_mongodb.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-sudo docker run -ti -d -p 27017:27017 -v $(pwd)/db:/data/db --name SmartHome_Database mongo
+INSTANCE_NAME=SmartHome_Database
+ROOT_DIR=/opt/SmartHome
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# check mongodb container state
+IS_RUNNING=$(docker inspect -f {{.State.Running}} $INSTANCE_NAME)
+
+# check if already running
+if [[ $IS_RUNNING == "true" ]]; then
+    echo MongoDB $INSTANCE_NAME is already running
+    exit 1
+fi
+
+docker run -ti -d -p 27017:27017 -v $ROOT_DIR/services/mongodb/db:/data/db --name $INSTANCE_NAME mongo
+docker ps -f name=INSTANCE_NAME
